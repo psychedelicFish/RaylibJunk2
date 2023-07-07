c834ea21cdc43a8af41f971edf0d5f4e04922587
BlizAce <blizace@gmail.com>
2023-07-07 15:19:34 +1000

    Started AI manager

---
+++ RaylibJunk2/AI/AIManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaylibJunk2.AI
+{
+	class AIManager
+	{
+		class NavagationNode
+		{
+			Transform transform;
+			bool passable;
+		}
+
+
+		List<NavagationNode> navagationMesh = new List<NavagationNode>();
+
+
+		public AIManager()
+		{
+
+		}
+
+		public void Update()
+		{
+
+		}
+
+		public void Draw()
+		{
+
+		}
+
+	}
+}
